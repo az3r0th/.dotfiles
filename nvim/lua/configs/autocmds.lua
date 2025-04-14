d75c1b49dfd5da2cce36ab028b910f418f9b3810
az3r0th <saurabhthe1000@gmail.com>
2025-04-15 00:18:05 +0530

    added avante

--- nvim/lua/configs/autocmds.lua
+++ nvim/lua/configs/autocmds.lua
@@ -6,3 +6,13 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   end,
 })
 
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  callback = function()
+    local dir = vim.fn.expand("%:p:h")
+    if vim.fn.isdirectory(dir) == 1 then
+      vim.cmd("lcd " .. vim.fn.fnameescape(dir))
+    end
+  end,
+})
+
